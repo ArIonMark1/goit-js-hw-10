aa652306ea4e1108bb3c8e6290c4b8907caaf12b
ArIonMark1 <65442269+ArIonMark1@users.noreply.github.com>
2023-04-29 16:40:36 +0000

    Deploying to gh-pages from @ ArIonMark1/goit-js-hw-10@314823d0434e4545bcbe7587371f8bbd5ddfe75f 🚀

--- index.53ba904a.js
+++
@@ -1,2 +0,0 @@
-fetch("https://www.pokeapi.co/api/v2/pokemon/174/");
-//# sourceMappingURL=index.53ba904a.js.map
--- index.b2601313.js
+++
@@ -1,2 +0,0 @@
-fetch("https://www.pokeapi.co/api/v2/pokemon/174/");
-//# sourceMappingURL=index.b2601313.js.map
