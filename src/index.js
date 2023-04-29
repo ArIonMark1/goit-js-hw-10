146fc94d26fd8df49ac539246c7ae985a97b5755
ArIonMark1 <andreydatsenko777@gmail.com>
2023-04-29 15:07:59 +0200

    my own task completed

--- src/index.js
+++ src/index.js
@@ -1,5 +1,43 @@
 import './css/styles.css';
 
 const DEBOUNCE_DELAY = 300;
+const contentBlock = document.querySelector('.country-info')
+// console.log('contentBlock ', contentBlock)
 
-fetch('https://www.pokeapi.co/api/v2/pokemon/174/')
+const header = document.createElement('h2')
+const body = document.createElement('div')
+
+// #################################################################
+
+const params = new URLSearchParams({
+    offset: 1,
+    limit: 5
+});
+
+const link = fetch(`https://pokeapi.co/api/v2/ability?${params}`)
+// #################################################################
+
+link
+    .then((response) => {
+        return response.json()
+    })
+    .then((pokemon) => { 
+        console.log('pokemon: ', pokemon)
+        console.log(pokemon.results[0].name)
+        console.log(pokemon.pokemon)
+
+        const blockList = pokemon.results.map(
+            (block, index) =>
+                `<p><span>ID: ${index + 1}; Pokemon name:</span>  <span>${block.name.toUpperCase()}</span></p>`
+        ).join('')
+        body.innerHTML = blockList;
+        
+    })
+    .catch((err) => { console.log(err) })
+
+
+// #################################################################
+
+contentBlock.style.backgroundColor = '#d4858e';
+contentBlock.style.color = 'white';
+contentBlock.append(header, body);
---
+++ src/pokemons.js
@@ -0,0 +1,89 @@
+import './css/styles.css'
+
+// #################################################################
+const pokemonBlock = document.querySelector('div[class=pokemon-card]')
+const pokemonList = document.querySelector('.pokemon-list');
+// #################################################################
+
+const params = new URLSearchParams({
+    offset: 1,
+    limit: 5
+});
+
+const link = fetch(`https://pokeapi.co/api/v2/pokemon`)
+// #################################################################
+link.then(response => {
+    if (!response.ok) {
+        throw new Error(response.status)
+    }
+    return response.json()
+})
+    .then(pokemon => {
+        console.log('pokemon: ', pokemon.results)
+
+        //****************************
+        const pokemonName = pokemon.results.map((pokemon) => {
+            return `
+                    <li class="pokemon-data">
+                        <h2>${pokemon.name}</h2>
+                        <p><a class='card-link' href="${pokemon.url}">Pokemon page</a></p>
+                    </li>
+            `
+        }).join('');
+
+        pokemonList.insertAdjacentHTML('beforeend', pokemonName)
+        //****************************
+    })
+    .catch(error => console.error(`${error}:: Wrong link to serwer!!! `))
+
+// #################################################################
+
+pokemonList.addEventListener('click', (event) => { 
+    event.preventDefault();
+    console.log(event.target.href);
+
+    pokemonCardMarkup(event.target.href);
+})
+
+function pokemonCardMarkup(link) { 
+    
+    fetch(link)
+        .then((response) => { 
+            if (!response.ok) { 
+                throw Error(':: Pokemon does not exist!!')
+            }
+            return response.json();
+        })
+        .then((data) => { 
+            pokemonBlock.innerHTML = markupOne(data);
+        })
+        .catch((err) => console.error(err))
+}
+
+function markupOne(pokemon) { 
+
+    const { name, sprites, weight, height, abilities: [...args] } = pokemon;
+
+        const pokemonAbil = args.map(element => {
+            return` <li class="list-group-item">${element.ability.name}</li> `
+        }).join('');
+    
+    return `
+        <div class="card">
+
+            <div class="card-img-top">
+                <img src="${sprites.front_default}" width='150px' height='150px' alt="${name}" />
+            </div>
+            <div class="card-body">
+
+                <h2 class="card-title">Name: ${name}</h2>
+                <p class="card-text">Weight: ${weight} kg.</p>
+                <p class="card-text">Height: ${height}'</p>
+                <p class="card-text"><b>Skills</b></p>
+                <ul class="list-group"></ul>
+                    ${pokemonAbil}
+                </ul>
+            </div>
+        </div>
+    `
+};
